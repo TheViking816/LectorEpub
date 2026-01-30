fc4deb6cb2d0a330ad571c0daed12dd733c358c2
TheViking816 <adrianlujan94@yahoo.es>
2026-01-30 23:00:55 +0100

    Updates

--- clear-db.js
+++
@@ -1,1 +0,0 @@
-console.log('Clearing IndexedDB...'); indexedDB.databases().then(dbs => dbs.forEach(db => indexedDB.deleteDatabase(db.name))); console.log('Database cleared. Please refresh the page.');
